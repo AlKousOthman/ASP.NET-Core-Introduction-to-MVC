97f19d18239c2e201d239e5057fad7b6a716ae58
new othman <new othman@OTHMAN>
2024-04-28 03:36:15 -0700

    Add project files.

---
+++ MVC-Day1/Controllers/CustomerController .cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Day1.Models;
+
+namespace MVC_Day1.Controllers
+{
+    public class CustomerController : Controller
+
+      
+  {
+        List<Customer> customerList =
+                [
+                    new Customer() { Id = 1, Name = "Nawaf", Email = "nawaf@hotmail.com"},
+                 new Customer() { Id = 2, Name = "Yousef", Email = "Yousef@hotmail.com"},
+                  new Customer() { Id = 3, Name = "Othman", Email = "Othman@gmail.com"}
+
+               ];
+
+        public IActionResult Index()
+        {
+            return View(customerList);
+        }
+
+       public  IActionResult Details(int id)
+        {
+            var customer = customerList.Find(c => c.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+    }
+}
---
+++ MVC-Day1/Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_Day1.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
